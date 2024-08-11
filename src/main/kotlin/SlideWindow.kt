826a60fa8be5f83f2656af03f7bb1736c06de5ec
Tablo <tablo_jhin19996@outlook.com>
2024-08-11 21:56:40 +0800

    平衡字符串r / l两种算法

--- src/main/kotlin/SlideWindow.kt
+++ src/main/kotlin/SlideWindow.kt
@@ -1,14 +1,3 @@
-/**
- * Description: 滑动窗口
- * Program:  SlideWindow Created on 2024/8/1 22:48
- *
- * @author <a href="mailto: tablojhin.lb@alibaba-inc.com">Tablo</a>
- * @version 1.0
- */
-class SlideWindow {
-
-}
-
 fun main() {
     println(lengthOfLongestSubString("abcabcbb"))
     println(lengthOfLongestSubString1("abcabcbb"))
@@ -21,16 +10,20 @@ fun main() {
     val third = System.currentTimeMillis()
     println("canCompleteCircuit1: ${third - second}ms")
 
+    println("balanceString1${balanceString1("WQWRQQQW")}")
+    println("balanceString${balanceString("WQWRQQQW")}")
+
+
 }
 
 fun minSubArrayLen(arr: IntArray, target: Int): Int {
     var result = Int.MAX_VALUE
-    var l = 0
     var sum = 0
-    arr.forEachIndexed { i, v ->
+    var l = 0
+    arr.forEachIndexed { r, v ->
         sum += v
         while (sum - arr[l] >= target) sum -= arr[l++]
-        if (sum >= target) result = minOf(result, i - l + 1)
+        if (sum >= target) result = minOf(r - l + 1, result)
     }
     return if (result == Int.MAX_VALUE) 0 else result
 }
@@ -48,13 +41,13 @@ fun lengthOfLongestSubString(s: String): Int {
 }
 
 fun lengthOfLongestSubString1(s: String): Int {
-    val map = IntArray(256) { -1 }
+    val map = IntArray(256) {-1}
     var result = 0
     var l = 0
-    s.forEachIndexed { i, c ->
-        l = maxOf(l, map[c.code] + 1)
-        result = maxOf(result, i - l + 1)
-        map[c.code] = i
+    s.forEachIndexed { r, c ->
+        l= maxOf(l, map[c.code]+1)
+        result = maxOf(result, r - l + 1)
+        map[c.code] = r
     }
     return result
 }
@@ -82,17 +75,16 @@ fun minWindows(s: String, target: String): String {
 
 fun canCompleteCircuit(gas: IntArray, cost: IntArray): Int {
     var sum = 0
-    var r = 0
-    var len = 0
-    val size = cost.size
+    val size = gas.size
     gas.forEachIndexed { l, v ->
+        var len = 0
+        var r = l
         while (sum >= 0) {
             if (len == size) return l
             r = (l + len++) % size
             sum += gas[r] - cost[r]
         }
         sum -= v - cost[l]
-        len--
     }
     return -1
 }
@@ -114,32 +106,73 @@ fun canCompleteCircuit1(gas: IntArray, cost: IntArray): Int {
     return -1
 }
 
-fun balanceString(string: String): Int {
-    var r = 0
-    val size = string.length
-    val arr = IntArray(size)
-    val count = IntArray(4)
+fun balanceString1(s: String): Int {
+    val size = s.length
     val require = size / 4
-    var len = size
-    string.forEachIndexed { index, c ->
-        val i = if (c == 'Q') 0 else if (c == 'W') 1 else if (c == 'E') 2 else 3
-        arr[index] = i
+    val count = IntArray(4)
+    val arr = s.map {
+        val i = mapToInt(it)
         count[i]++
+        i
     }
+    var r = 0
+    var result = size
     arr.forEachIndexed { l, v ->
-        while (!ok(r - l, count, require) && r < size) count[arr[r++]]--
-        if (ok(r - l, count, require)) len = minOf(r - l, len)
-        count[arr[l]]++
+        while ((!(ok(count, r - l, require)) && r < size)) count[arr[r++]]--
+        if (ok(count, r - l, require)) result = minOf(result, r - l)
+        count[v]++
     }
-    return len
+    return result
 }
 
-fun ok(len: Int, count: IntArray, require: Int): Boolean {
+fun balanceString(string: String): Int {
+    val size = string.length
+    val require = size / 4
+    var l = 0
+    val count = IntArray(4)
+    var result = size
+    val list = string.map {
+        val i = mapToInt(it)
+        count[i]++
+        i
+    }
+    list.forEachIndexed { r, v ->
+        count[v]--
+        if (ok(count, r - l + 1, require)) {
+            do count[list[l++]]++
+            while (ok(count, r - l + 1, require) && l <= r)
+            if (!ok(count, r - l + 1, require)) count[list[--l]]--
+            result = minOf(result, r - l + 1)
+        }
+    }
+    return result
+}
+
+fun ok(count: IntArray, len: Int, require: Int): Boolean {
     var length = len
     for (i in 0 until 4) {
-        if (require < count[i]) return false
+        if (count[i] > require) return false
         length -= require - count[i]
     }
     return length == 0
 }
 
+private fun mapToInt(it: Char) = if (it == 'Q') 0 else if (it == 'W') 1 else if (it == 'E') 2 else 3
+
+
+fun subArraysWithDistinct(arr: IntArray, k: Int): Int {
+    return collect(arr, k) - collect(arr, k - 1)
+}
+
+fun collect(arr: IntArray, k: Int): Int {
+    var collected = 0
+    val count = IntArray(arr.size)
+    var l = 0
+    var result = 0
+    arr.forEachIndexed { r, v ->
+        if (count[v]++ == 0) collected++
+        if (collected > k) while (--count[arr[l++]] == 0) collected--
+        result += r - l + 1
+    }
+    return result
+}
